be1f6f715cb55dbf9ca833898b757a28d23c9227
GUTT-JuanVazquez <71057053+GUTT-JuanVazquez@users.noreply.github.com>
2021-04-12 14:27:27 -0500

    Cambios diseño

--- src/app/Component/card-temp/card-temp.component.ts
+++ src/app/Component/card-temp/card-temp.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { environment } from 'src/environments/environment.prod';
 import { ServiciosService } from 'src/app/servicios.service';
 import { Router } from '@angular/router';
+import { InterTemperaturaComponent } from '../inter-temperatura/inter-temperatura.component';
 
 @Component({
   selector: 'app-card-temp',
@@ -13,7 +14,7 @@ import { Router } from '@angular/router';
 export class CardTempComponent implements OnInit {
   @Input() sensores:Temperatura
   public invited:Boolean =  environment.invited;
-  constructor(private api: ServiciosService) {
+  constructor(private api: ServiciosService, private interTemp: InterTemperaturaComponent) {
   }
 
   ngOnInit(): void {
@@ -24,6 +25,7 @@ export class CardTempComponent implements OnInit {
     console.log(request)
     this.api.deletedevice(request).subscribe(data => {
       console.log("hecho delete/device")
+      this.interTemp.TEMPERATURA()
       console.log(data)
     }, error =>{
       console.log("Eliminar Temperatura error")
