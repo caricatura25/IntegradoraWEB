22a2b91ef5e3848c5de48f556472f53cbe45bd6a
RosaMedina28 <rosamedina1008@gmail.com>
2021-04-14 15:09:58 -0500

    ee

--- src/app/Component/inter-humedad/inter-humedad.component.ts
+++ src/app/Component/inter-humedad/inter-humedad.component.ts
@@ -4,6 +4,7 @@ import { Humedad } from 'src/app/Interfaces/humedad';
 import { environment } from 'src/environments/environment.prod';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
+import { Dato } from 'src/app/Interfaces/dato';
 
 
 @Component({
@@ -39,7 +40,7 @@ export class InterHumedadComponent implements OnInit {
   peticiondatos(){
     console.log("realizabdo peticion")
     const request = {dispositivo_id: 4}
-    this.api.datos(request).subscribe(data => {
+    this.Hum.datos(request).subscribe(data => {
       console.log("hecho")
       this.datos = data.registros
       console.log(data)
@@ -48,6 +49,8 @@ export class InterHumedadComponent implements OnInit {
       console.log(error)
     });
   }
+
+  
   checkToken(){
     console.log("Verificando Token-- CheckToken()")
     
